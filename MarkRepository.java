041d56b791b6c23b36e2a376ded307ffe27ee802
JuanAvendano95 <168042705+JuanAvendano95@users.noreply.github.com>
2026-06-21 11:11:17 -0700

    Create MarkRepository interface for Mark entity

---
+++ MarkRepository.java
@@ -0,0 +1,7 @@
+package com.tiendasara.Tienda.Sara.services;
+
+import com.tiendasara.Tienda.Sara.models.Mark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarkRepository extends JpaRepository<Mark, Integer> {
+}
